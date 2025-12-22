3d6e05964c387e3cdb316c65785e9c73b4819553
CyberMind-FR <gandalf@Gk2.net>
2025-12-22 08:39:49 +0100

    Fix ubus

--- htdocs/luci-static/resources/netdata-dashboard/api.js
+++ htdocs/luci-static/resources/netdata-dashboard/api.js
@@ -2,19 +2,39 @@
 'require baseclass';
 'require rpc';
 
+/**
+ * Netdata Dashboard API
+ * Package: luci-app-netdata-dashboard
+ * RPCD object: luci.netdata-dashboard
+ */
+
 var callStatus = rpc.declare({
-	object: 'luci.netdata',
+	object: 'luci.netdata-dashboard',
 	method: 'status',
 	expect: { }
 });
 
 var callInfo = rpc.declare({
-	object: 'luci.netdata',
+	object: 'luci.netdata-dashboard',
 	method: 'info',
 	expect: { }
 });
 
+var callCharts = rpc.declare({
+	object: 'luci.netdata-dashboard',
+	method: 'charts',
+	expect: { charts: [] }
+});
+
+var callAlarms = rpc.declare({
+	object: 'luci.netdata-dashboard',
+	method: 'alarms',
+	expect: { alarms: [] }
+});
+
 return baseclass.extend({
 	getStatus: callStatus,
-	getInfo: callInfo
+	getInfo: callInfo,
+	getCharts: callCharts,
+	getAlarms: callAlarms
 });
